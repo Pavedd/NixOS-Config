3be3be023e50bac72b0879d53d126fb2203eb5f6
Endersoul46 <paul.theodor.maier@gmail.com>
2026-03-12 13:22:01 +0100

    godot nvim support update

--- nonNix/nvim_godot_conn.sh
+++ nonNix/nvim_godot_conn.sh
@@ -1,22 +1,29 @@
-
 #!/usr/bin/env bash
 
 PIPE="$XDG_RUNTIME_DIR/godot.pipe"
-FILE="$1"
-LINE="${2:-1}"   # Defaults to 1
-COL="${3:-1}"    
+FILE=$(printf %q "$1")
+LINE="${2:-1}"   # default to 1 if empty
+COL="${3:-1}"    # default to 1 if empty
+
 
 if [ -z "$FILE" ]; then
     echo "Usage: $0 <file> [line] [col]"
     exit 1
 fi
 
-# checks if nvim istance is running on pipe
-if nvim --server "$PIPE" --remote-expr "1" >/dev/null 2>&1; then
-    # opens File and goes to line
-    nvim --server "$PIPE" --remote-send "<Esc>:edit $(printf '%q' "$FILE")<CR>:call cursor($LINE, $COL)<CR>"
-else
-    # new instance and goes to line
-    kitty nvim --listen "$PIPE" "+call cursor($LINE, $COL)" "$FILE" &
+# Only generate cursor command if either line or col is NOT 1
+CURSOR_CMD=""
+if [ "$LINE" -ne 1 ] || [ "$COL" -ne 1 ]; then
+    CURSOR_CMD=":call cursor($LINE, $COL)<CR>"
 fi
 
+# checks if nvim instance is running on pipe
+if nvim --server "$PIPE" --remote-expr "1" >/dev/null 2>&1; then
+    nvim --server "$PIPE" --remote-send "<C-\><C-n>:n $FILE<CR>$CURSOR_CMD"
+else
+    if [ -n "$CURSOR_CMD" ]; then
+        kitty nvim --listen "$PIPE" "+call cursor($LINE, $COL)" "$FILE" &
+    else
+        kitty nvim --listen "$PIPE" "$FILE" &
+    fi
+fi
