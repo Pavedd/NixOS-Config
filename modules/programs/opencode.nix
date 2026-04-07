f9b5775e5134580a2dd76a68a30ddab5fd7e72f2
Endersoul46 <paul.theodor.maier@gmail.com>
2026-04-07 03:55:04 +0200

    opencode

---
+++ modules/programs/opencode.nix
@@ -0,0 +1,45 @@
+{
+  flake.homeModules.opencode = {pkgs, config, ... }: {
+    programs.opencode = {
+      enable = true;
+      settings = 
+{
+  model = "ollamaRemote/gpt-oss:20b";    # remote Ollama model on your LAN
+  provider = {
+    astolfo = {
+      npm = "@ai-sdk/openai-compatible";
+      name = "astolfo desktop provider";
+      options = {
+        baseURL = "http://127.0.0.0:11434/v1";
+      };
+      models = {
+        "gpt-oss:20b" = {
+          name = "gpt oss 20b";
+          options = {
+          };
+        };
+        "deepseek-coder-v2:16b" = {
+          name = "deepseek coder 16b";
+          options = {
+          };
+        };
+ 
+      };
+    };
+  };
+
+  # Web capabilities
+  tools = {
+    webfetch = true;   # allow fetching content from URLs
+    websearch = true;  # allow performing web searches
+  };
+
+  # AI behavior options
+  autoshare = false;        # keep prompts private
+  autoupdate = true;        # update OpenCode automatically
+
+      };
+
+    };
+  };
+}
