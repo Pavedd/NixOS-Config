e39d5c60583dc1f16a56d1b4eb0f9244fb18fe3d
Endersoul46 <paul.theodor.maier@gmail.com>
2026-04-07 03:06:01 +0200

    some other ai fumbling

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -4,8 +4,12 @@
       enable = true;
       host = "0.0.0.0";
       port = 11434; 
-      package = pkgs.ollama-rocm;
+      package = pkgs.unstable.ollama-rocm;
     };
     networking.firewall.allowedTCPPorts = [ 11434 ]; 
+    environment.systemPackages = with pkgs; [
+      open-webui
+    ];
+ 
   };
 }
